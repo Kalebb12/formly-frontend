9a53ed0ca32cf16fde4ba7ff8641b76b98814b53
Caleb <2021.cee.jay@gmail.com>
2025-12-30 16:06:51 +0100

    feat: add Label component and integrate it into Forms for improved form handling

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -4,6 +4,20 @@ import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
 import { getUserForms } from "@/api/forms";
 import { useNavigate } from "react-router";
+import { FilePlus } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const Forms = () => {
   const { user } = useSelector((state: RootState) => state.auth);
@@ -14,15 +28,13 @@ const Forms = () => {
 
   const navigate = useNavigate();
   const shadowCards = Array(6).fill(null);
-
-  console.log(user,data)
   return (
     <div>
       <div className="flex justify-between items-center">
         <h2 className="font-medium text-xl">
           Welcome back {user?.name} &#x1F44B;
         </h2>
-        <Button className="bg-blue-500 hover:bg-blue-600"  onClick={()=> navigate("/create-form")}>Create Form</Button>
+        {/* <Button className="bg-blue-500 hover:bg-blue-600"  onClick={()=> navigate("/create-form")}>Create Form</Button> */}
       </div>
 
       <div className="mt-4">
@@ -35,8 +47,7 @@ const Forms = () => {
             <div
               key={index}
               className="animate-pulse bg-black/80 h-[180px] p-4 space-y-3 relative rounded shadow"
-            >
-            </div>
+            ></div>
           ))}
         </div>
       )}
@@ -44,6 +55,48 @@ const Forms = () => {
 
       {data && (
         <div className="grid grid-cols-3 gap-4 mt-4">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant="outline"
+                className="bg-white flex flex-col gap-2 items-center justify-center h-full w-full p-4 space-y-3 rounded cursor-pointer shadow"
+              >
+                <FilePlus strokeWidth={1} className="size-10" />
+                <span className="font-medium">Create New Form</span>
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <form>
+                <DialogHeader>
+                  <DialogTitle>Create New Form</DialogTitle>
+                  <DialogDescription>
+                    Create new form here. Click create form when you&apos;re
+                    done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4">
+                  <div className="grid gap-3">
+                    <label htmlFor="name-1">Name</label>
+                    <Input id="name-1" name="name" placeholder="Form name" />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="username-1">Description</Label>
+                    <Textarea
+                      id="description"
+                      rows={4}
+                      placeholder="Enter form description"
+                    />
+                  </div>
+                </div>
+                <DialogFooter className="mt-3">
+                  <DialogClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit">Create form</Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
           {data.map((form) => (
             <div
               key={form._id}
